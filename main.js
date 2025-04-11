f16ccf3c0a98e2eadfb8876cdd23756b66e10263
Yazeed990 <btmk.monk@gmail.com>
2025-04-12 00:59:26 +0300

    website is ready and responsive, with awesome futures

--- main.js
+++ main.js
@@ -3,9 +3,30 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
-    primaryNav.hasAttribute("data-visible")
-        ? navToggle.setAttribute("aria-expanded", false)
-        : navToggle.setAttribute("aria-expanded", true);
-    primaryNav.toggleAttribute("data-visible");
-    primaryHeader.toggleAttribute("data-overlay");  
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
 });
+
+
+const slider = new A11YSlider(document.querySelector('.slider'), {
+  adaptiveHeight: false,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  centerMode: true, 
+  arrows: false,
+  responsive: {
+    
+    800: {
+      dots: false, // dots enabled 1280px and up
+    }
+  }
+});
+
+
+
+
+
